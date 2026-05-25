346c7a3d7df0b3ecec8c41ac00bfe6d5f05a9158
NewMaster27 <newmasterone27@gmail.com>
2026-05-25 19:41:21 +0000

    Update script to use anonproxy instead of nclient

--- make_bin.sh
+++ make_bin.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-chmod +x nclient.git && mv nclient.git nclient && mv nclient ipblocked /usr/bin && nclient help
+chmod +x anonproxy.git && mv anonproxy.git anonproxy && mv anonproxy /usr/bin && anonproxy help
